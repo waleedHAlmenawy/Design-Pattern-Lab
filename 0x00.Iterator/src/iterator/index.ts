dc525814d21241f609380eb418f0d819e7fe33c5
waleedHAlmenawy <waleed.almenawy@outlook.com>
2024-04-22 10:31:10 +0200

    Updated

--- 0x00.Iterator/src/iterator/index.ts
+++ 0x00.Iterator/src/iterator/index.ts
@@ -24,21 +24,24 @@ console.log("%cIterator with the basic form", "color: red; font-size: 20px");
 const _rangeV1 = rangeV1(1, 10, 1);
 
 for (const value of _rangeV1) {
-    console.log(value)
+  console.log(value);
 }
 
 /////////////////////////////////////////////////////////
 
-console.log("%cIterator with the genrator function", "color: red; font-size: 20px");
+console.log(
+  "%cIterator with the generator function",
+  "color: red; font-size: 20px"
+);
 
 function* rangeV2(start: number, end: number, steps = 1) {
-    for(let i = start; i <= end; i += steps) {
-        yield i;
-    }
+  for (let i = start; i <= end; i += steps) {
+    yield i;
+  }
 }
 
 const _rangeV2 = rangeV2(1, 10, 2);
 
 for (const value of _rangeV2) {
-    console.log(value)
+  console.log(value);
 }
