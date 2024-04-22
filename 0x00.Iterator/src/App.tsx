50a9ffcdf917d6505ba9be3da8cf7b4c6505352c
waleedHAlmenawy <waleed.almenawy@outlook.com>
2024-04-22 10:21:23 +0200

    Upload labs

---
+++ 0x00.Iterator/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="read-the-docs">
+        Look at the console :)
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ 0x00.Iterator/src/iterator/index.ts
@@ -0,0 +1,44 @@
+function rangeV1(start: number, end: number, steps: 1) {
+  let currentStep = start;
+
+  return {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          if (currentStep <= end) {
+            let result = currentStep;
+            currentStep += steps;
+
+            return { value: result, done: false };
+          } else {
+            return { value: undefined, done: true };
+          }
+        },
+      };
+    },
+  };
+}
+
+console.log("%cIterator with the basic form", "color: red; font-size: 20px");
+
+const _rangeV1 = rangeV1(1, 10, 1);
+
+for (const value of _rangeV1) {
+    console.log(value)
+}
+
+/////////////////////////////////////////////////////////
+
+console.log("%cIterator with the genrator function", "color: red; font-size: 20px");
+
+function* rangeV2(start: number, end: number, steps = 1) {
+    for(let i = start; i <= end; i += steps) {
+        yield i;
+    }
+}
+
+const _rangeV2 = rangeV2(1, 10, 2);
+
+for (const value of _rangeV2) {
+    console.log(value)
+}
